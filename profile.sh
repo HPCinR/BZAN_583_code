033a53bbad9fe2b49fc431e62fa5816a6158fedf
George Ostrouchov <gostrouc@utk.edu>
2024-03-25 14:51:06 -0400

    added mem

--- profile.sh
+++ profile.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name utk
 #SBATCH --account=bckj-delta-cpu
 #SBATCH --partition=cpu
-#SBATCH --tasks=1
-#SBATCH --time 00:00:10
+#SBATCH --mem=16g
+#SBATCH --cpus-per-task=1
+#SBATCH --time 00:00:30
 #SBATCH -e ./utk.e
 #SBATCH -o ./utk.o
 
