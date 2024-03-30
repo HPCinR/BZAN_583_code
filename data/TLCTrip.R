2a3bef660ef726587b00d6a847a62788781e3e08
George Ostrouchov <gostrouc@utk.edu>
2024-03-30 15:17:41 -0400

    curl

--- data/TLCTrip.R
+++ data/TLCTrip.R
@@ -1,6 +1,6 @@
 suppressMessages(library(lubridate, quietly = TRUE))
 
-get_TLCData = function(start, end, name, dir, url, dest, method = "wget") {
+get_TLCData = function(start, end, name, dir, url, dest, method = "wget", quiet = TRUE) {
   start = ym(start)
   end = ym(end)
 
@@ -13,15 +13,16 @@ get_TLCData = function(start, end, name, dir, url, dest, method = "wget") {
     if(!dir.exists(dest_dir)) dir.create(dest_dir)
 
     system.time({
-    download.file(file_url, paste0(dest_dir, "/", file_name), method = method, quiet = TRUE)
+    download.file(file_url, paste0(dest_dir, "/", file_name), method = method, quiet = quiet)
     })
   }
 }
 
 dest = "/projects/bckj/TLC"
+# dest = "~/UTK/NYTaxi/"
 url = "https://d37ci6vzurychx.cloudfront.net"
-get_TLCData(start = "2009-01", end = "2009-02", name = 'yellow_tripdata_',
-            dir = 'trip-data', url = url, dest = dest)
+get_TLCData(start = "2009-03", end = "2009-03", name = 'yellow_tripdata_',
+            dir = 'trip-data', url = url, dest = dest, method = "curl", quiet = TRUE)
 # get_TLCData(start = "2009-01", end = "2009-02", name = 'green_tripdata_',
 #             dir = 'trip-data', url = url, dest = dest)
 # get_TLCData(start = "2009-01", end = "2009-02", name = 'fhv_tripdata_',
