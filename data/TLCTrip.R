8eae32cd1a76ff04ba9ba16772a2eb2a19542a4f
George Ostrouchov <gostrouc@utk.edu>
2024-03-30 23:27:27 -0400

    rewrite as lapply

--- data/TLCTrip.R
+++ data/TLCTrip.R
@@ -1,26 +1,35 @@
 suppressMessages(library(lubridate, quietly = TRUE))
 
-get_TLCData = function(start, end, name, dir, url, dest, method = "wget", quiet = TRUE) {
-  start = ym(start)
-  end = ym(end)
-
-  char_dates = as.character(seq(start, end, by = "months"))
-  for (d in char_dates) {
-    yr = year(d)
-    file_name = paste0(name, yr, '-', sprintf('%02d', month(d)), '.parquet')
-    file_url = paste0(url, '/', dir, '/', file_name)
-    dest_dir = paste0(dest, "/", yr)
-    if(!dir.exists(dest_dir)) dir.create(dest_dir)
-
-    system.time({
-    download.file(file_url, paste0(dest_dir, "/", file_name), method = method, quiet = quiet)
-    })
-  }
+#' Function to wget and write one yr-month
+#' @param d
+#' Character "YYYY-MM"
+#' @param name
+#' Character file name base (such as 'yellow_tripdata_')
+#' @param dest
+#' Character destination directory
+#' @param url
+#' Character URL of data location
+tlcYM_get = function(d, name, dest, url) {
+  yr = year(d)
+  file_name = paste0(name, yr, '-', sprintf('%02d', month(d)), '.parquet')
+  file_url = paste0(url, '/', file_name)
+  dest_dir = paste0(dest, "/", yr)
+  if(!dir.exists(dest_dir)) dir.create(dest_dir)
+  
+  print(file_url)
+  download.file(file_url, paste0(dest_dir, "/", file_name), method = "curl",
+                quiet = TRUE)
 }
 
-dest = "/projects/bckj/TLC"
-# dest = "~/UTK/NYTaxi/"
-url = "https://d37ci6vzurychx.cloudfront.net"
-get_TLCData(start = "2021-01", end = "2023-12", name = 'yellow_tripdata_',
-            dir = 'trip-data', url = url, dest = dest, method = "curl", quiet = TRUE)
+start = ym("2021-01")
+end = ym("2023-12")
+
+## construct vector of yr-month requests
+char_dates = as.character(seq(start, end, by = "months"))
+
+lapply(char_dates, tlcYM_get, 
+       name = 'yellow_tripdata_', 
+       dest = "/projects/bckj/TLC",
+       url = "https://d37ci6vzurychx.cloudfront.net/trip-data")
+
 
