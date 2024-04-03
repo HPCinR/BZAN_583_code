67b40d827482b70887b531a3e1ed253ae1dc2295
George Ostrouchov <gostrouc@utk.edu>
2024-04-03 16:44:32 -0400

    rprof added

--- preallocate.R
+++ preallocate.R
@@ -1,17 +1,25 @@
 m = 1e3
 n = 1e5
 mat = matrix(runif(m*n), nrow = m)
-system.time({
+## system.time({
+Rprof()
   result1 = NULL
   for(i in seq_len(n)) {
     newres = mean(mat[, i])
     result1 = c(result1, newres)
-  }})
+  }
+Rprof(NULL)
+summaryRprof()
+##  })
 
-system.time({
+##system.time({
+  Rprof()
   result2 = vector("double", n)
   for(i in seq_len(n)) {
     result2[i] = mean(mat[, i])
-  }})
+  }
+  Rprof(NULL)
+  summaryRprof()
+##  })
 
 all.equal(result1, result2)
