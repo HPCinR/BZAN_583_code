cab61a797bfdaaa7941eddc3ab2cba927aa99967
George Ostrouchov <gostrouc@utk.edu>
2024-04-07 00:14:11 -0400

    rewrite for interactive

--- data/TLCTrip.R
+++ data/TLCTrip.R
@@ -1,26 +1,34 @@
-suppressMessages(library(lubridate, quietly = TRUE))
 
-#' A wrapper for `download.file()` specific to TLC data to `wget` one
-#' yr-month to a destination directory. Since `download.file()` engages `wget`
-#' with a `system()` call, generating these calls to `tlcYM_get()` can be 
-#' completely parallel and the data never touches R. 
-#' @param d
-#' Character "YYYY-MM-DD" giving the required date.
+#' tlc_getYM: Gets one month of TLC yellow taxi data.
+#' 
+#' @param ym
+#' Character "YYYY-MM" giving the year and month to be retrieved.
 #' @param name
-#' Character file name base (such as 'yellow_tripdata_') from the TLC 
+#' Character file name base (such as 'yellow_tripdata') from the TLC 
 #' repository documentation.
 #' @param dest
 #' Character destination directory path.
 #' @param url
 #' Character URL of data source location.
 #' 
+#' @details
+#' Uses `method = wget` to get one month of TLC yellow taxi data into the
+#' `dest` directory. Since `download.file()` engages `wget`
+#' with a `system()` call, generating these calls to `tlcYM_get()` can be 
+#' completely parallel and the data never touches R. 
+#' 
 #' @returns 
 #' Invisibly, returns the character URL/file combination used in data retrieval.
-tlcYM_get = function(d, name, dest, url) {
-  yr = year(d)
+#' 
+tlc_get_ym = function(ym, name = "yellow_tripdata", 
+                     dest = "/projects/bckj/TLC_yellow",
+                     url = "https://d37ci6vzurychx.cloudfront.net/trip-data") {
+  d = lubridate::ym(ym)
+  yr = lubridate::year(d)
+  month = lubridate::month(d)
 
   ## Construct source url/file
-  file_name = paste0(name, yr, '-', sprintf('%02d', month(d)), '.parquet')
+  file_name = paste0(name, "_", yr, '-', sprintf('%02d', month), '.parquet')
   file_url = paste0(url, '/', file_name)
   
   ## Construct destination directory
@@ -32,11 +40,11 @@ tlcYM_get = function(d, name, dest, url) {
   invisible(file_url)
 }
 
-## construct vector of yr-month-day requests
-char_dates = as.character(seq(ym("2021-01"), ym("2023-12"), by = "months"))
+tlc_get_range = function(first, last, cores = 1) {
+  ## construct vector of yr-month-day requests
+  dates = seq(lubridate::ym(first), lubridate::ym(last), "months")
+  mclapply(dates, tlc_get_ym, mc.cores = cores)
+}
 
-mclapply(char_dates, tlcYM_get, 
-       name = 'yellow_tripdata_', 
-       dest = "/projects/bckj/TLC_yellow",
-       url = "https://d37ci6vzurychx.cloudfront.net/trip-data",
-       mc.cores = 4)
+tlc_get_range("2021-01", "2022-12", cores = 1)
+
