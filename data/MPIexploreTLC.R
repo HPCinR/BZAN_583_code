3507dc1a7d256067d5d8f2efcf7d8ecafe1578a0
George Ostrouchov <gostrouc@utk.edu>
2024-04-10 11:11:02 -0400

    try gather

--- data/MPIexploreTLC.R
+++ data/MPIexploreTLC.R
@@ -7,12 +7,8 @@ suppressMessages(library(pryr))
 suppressMessages(library(parallel))
 
 memuse::Sys.procmem()
-arrow_info()
-
-system2("tree", "/projects/bckj/TLC_yellow/year=2009")
 
 print("opening dataset")
-
 tlc = arrow::open_dataset("/projects/bckj/TLC_yellow/year=2009")
 tlc
 
@@ -26,7 +22,13 @@ my_tlc2009 = lapply(my_months, read_tlc_month, tlc = tlc)
 my_tlc2009 = do.call(rbind, my_tlc2009)
 
 Total_mean = comm.mean(my_tlc2009$Total_Amt)
+comm.print(Total_mean)
 
+tlc2009 = allgather(my_tlc2009)
+comm.cat("my_dim:", dim(my_tlc2009), "\n")
+comm.cat("dim:", dim(tlc2009), "\n")
+
+Total_mean = mean(tlc2009$Total_Amt)
 comm.print(Total_mean)
 
 finalize()
