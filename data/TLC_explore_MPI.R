838f0a82d5f2d2d5ae8a0d339195a0390b373b70
George Ostrouchov <gostrouc@utk.edu>
2024-04-10 00:27:39 -0400

    open only one year

--- data/TLC_explore_MPI.R
+++ data/TLC_explore_MPI.R
@@ -13,14 +13,13 @@ system2("tree", "/projects/bckj/TLC_yellow/")
 
 print("opening dataset")
 
-tlc = arrow::open_dataset("/projects/bckj/TLC_yellow/")
+tlc = arrow::open_dataset("/projects/bckj/TLC_yellow/year=2009")
 tlc
 
-months = 1:12
-my_months = comm.chunk(months, form = "vector")
+my_months = comm.chunk(12, form = "vector")
 
 read_tlc_month = function(m, tlc) {
-  tlc %>% filter(year == 2009, month == m) %>% collect()
+  tlc %>% filter(month == m) %>% collect()
 }
 
 my_tlc2009 = lapply(my_months, read_tlc_month, tlc = tlc)
