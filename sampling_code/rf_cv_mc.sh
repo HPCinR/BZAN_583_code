74a99f764d3d1e458b18ddd400cd18d6816ee847
George Ostrouchov <gostrouc@utk.edu>
2024-04-17 14:46:20 -0400

    convert to slurm

--- sampling_code/rf_cv_mc.sh
+++ sampling_code/rf_cv_mc.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#PBS -N rf
-#PBS -l select=1:ncpus=128,walltime=00:50:00
-#PBS -q qexp
-#PBS -e rf_cv.e
-#PBS -o rf_cv.o
+#SBATCH --job-name utk
+#SBATCH --account=bckj-delta-cpu
+#SBATCH --partition=cpu
+#SBATCH --mem=20g
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time 00:10:00
+#SBATCH -e ./utk.e
+#SBATCH -o ./utk.o
 
-cd ~/KPMS-IT4I-EX/code
 pwd
 
-module load R
+module load r
 echo "loaded R"
 
 time Rscript rf_cv_serial.R
 time Rscript rf_cv_mc.R --args 8
 time Rscript rf_cv_mc.R --args 16
-time Rscript rf_cv_mc.R --args 32
-time Rscript rf_cv_mc.R --args 64
-time Rscript rf_cv_mc.R --args 128
