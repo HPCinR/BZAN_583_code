f1c543976cb593453e77f5caa0f2bfedcf02877b
George Ostrouchov <gostrouc@utk.edu>
2024-04-22 10:17:39 -0400

    mod for R

--- torch/torch.sh
+++ torch/torch.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#SBATCH --mem=64g
+#SBATCH --mem=32g
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16     # <- match to OMP_NUM_THREADS
 #SBATCH --partition=gpuA100x4-interactive
 #SBATCH --time=00:10:00
-#SBATCH --account=account_name    # <- match to a "Project" returned by the "accounts" command
-#SBATCH --job-name=tf_anaconda
+#SBATCH --account=bckj-delta-gpu
+#SBATCH --job-name=utk
+#SBATCH -e ./utk.e
+#SBATCH -o ./utk.o
 ### GPU options ###
 #SBATCH --gpus-per-node=1
 #SBATCH --gpus-per-task=1
@@ -16,13 +18,17 @@
 module purge # drop modules and explicitly load the ones needed
              # (good job metadata and reproducibility)
 
-module load anaconda3_gpu
+#module load anaconda3_gpu
+module load r
 module list  # job documentation and metadata
 
 echo "job is starting on `hostname`"
 
-which python3
-conda list tensorflow
-srun python3 \
-  tf_gpu.py
+#which python3
+#conda list tensorflow
+#srun python3 \
+#  tf_gpu.py
+
+Rscript -e "torch_tensor(1, device = "cuda")"
+
 exit
