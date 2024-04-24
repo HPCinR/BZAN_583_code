33357e589819d3bab9b282ba31b63ad13ffa419f
George Ostrouchov <gostrouc@utk.edu>
2024-04-24 11:58:04 -0400

    add mpi codes

---
+++ mpi/hello_world.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name utk
+#SBATCH --account=bckj-delta-cpu
+#SBATCH --partition=cpu
+#SBATCH --mem=20g
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --tasks-per-node=8
+#SBATCH --time 00:10:00
+#SBATCH -e ./utk.e
+#SBATCH -o ./utk.o
+
+pwd
+module load r
+module list
+
+time mpirun -np 8 Rscript hello_world.R
